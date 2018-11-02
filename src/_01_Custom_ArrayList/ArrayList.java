b23423e536b8a4393fb66fb3656223bd3306e026
peter <League@AdministorsiMac.attlocal.net>
2018-11-01 19:57:53 -0700

    STACKED
    THE STEAKS ARE STAKED AND THE STACKS ARE STACKED
    10/10 commit message summary

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -1,35 +1,44 @@
 package _01_Custom_ArrayList;
 
+import java.util.Stack;
+
 @SuppressWarnings("unchecked")
 
-public class ArrayList <T>{
-	
+public class ArrayList<T> {
+	Stack<T> stacked = new Stack<>();
+
 	public ArrayList() {
+
 	}
-	
+
 	public T get(int loc) throws IndexOutOfBoundsException {
-		
-		return null;
+		return stacked.get(loc);
 	}
-	
+
 	public void add(T val) {
-		
+		stacked.push(val);
 	}
-	
+
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+		stacked.insertElementAt(val, loc);
 	}
-	
+
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		stacked.set(loc, val);
 	}
-	
+
 	public void remove(int loc) throws IndexOutOfBoundsException {
-		
+		stacked.remove(loc);
 	}
-	
+
 	public boolean contains(T val) {
-		
+		if (stacked.contains(val)) {
+			return true;
+		}
 		return false;
+	}
+
+	public int size() {
+		return stacked.size();
 	}
 }
